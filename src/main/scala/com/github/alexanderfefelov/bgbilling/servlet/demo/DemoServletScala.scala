f9ba1fa471febaaeb0e7c7e739a3f867e97288d7
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-12-30 10:01:05 +0300

    Use context logging

--- src/main/scala/com/github/alexanderfefelov/bgbilling/servlet/demo/DemoServletScala.scala
+++ src/main/scala/com/github/alexanderfefelov/bgbilling/servlet/demo/DemoServletScala.scala
@@ -4,44 +4,82 @@ import org.apache.log4j.Logger
 import javax.servlet.http.HttpServlet
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import ru.bitel.common.logging.NestedContext
 
 class DemoServletScala extends HttpServlet {
 
   override def init(): Unit = {
-    logger.trace("init")
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("init")
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   override def destroy(): Unit = {
-    logger.trace("destroy")
-    super.destroy()
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("destroy")
+      super.destroy()
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   override def doGet(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    logger.trace("doGet")
-    val writer = response.getWriter
-    writer.println("Hello, World!")
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("doGet")
+      val writer = response.getWriter
+      writer.println("Hello, World!")
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   override def doPost(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    logger.trace("doPost")
-    super.doPost(request, response)
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("doPost")
+      super.doPost(request, response)
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   override def doPut(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    logger.trace("doPut")
-    super.doPut(request, response)
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("doPut")
+      super.doPut(request, response)
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   override def doDelete(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    logger.trace("doDelete")
-    super.doDelete(request, response)
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("doDelete")
+      super.doDelete(request, response)
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   override def doHead(request: HttpServletRequest, response: HttpServletResponse): Unit = {
-    logger.trace("doHead")
-    super.doHead(request, response)
+    try {
+      NestedContext.push(LOG_CONTEXT)
+      logger.trace("doHead")
+      super.doHead(request, response)
+    } finally {
+      NestedContext.pop()
+    }
   }
 
   val logger: Logger = Logger.getLogger(this.getClass)
+
+  val LOG_CONTEXT = "servlet"
 
 }
